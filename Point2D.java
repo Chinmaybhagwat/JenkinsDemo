23a99e54a774ac215889d1451ea44b19e8807323
Chinmaybhagwat <35619926+Chinmaybhagwat@users.noreply.github.com>
2021-11-30 16:36:10 +0530

    Add files via upload

---
+++ Point2D.java
@@ -0,0 +1,38 @@
+package com.Point;
+
+public class Point2D {
+
+private int x,y;
+	
+	private Point2D() {
+		this.x = 0;
+		this.y = 0;
+	}
+	public Point2D(int x, int y){
+		this.x = x;
+		this.y = y;
+	}
+	
+	public String getDetails() {
+		return "Point (" + x + "," + y +")";
+	}
+	
+	public boolean isEqual(Point2D pt) {
+		if(this.x == pt.x && this.y == pt.y) {
+			return true;
+		}
+		return false;
+	}
+	
+	public Point2D createNewPoint(int xCords, int yCords) {
+		return new Point2D(xCords + this.x , yCords + this.y);
+	}
+	public double calcDistance(Point2D pt) {
+		return Math.sqrt(Math.pow(pt.x - this.x,2) + Math.pow(pt.y - this.y,2));
+	}
+
+
+
+	}
+
+
